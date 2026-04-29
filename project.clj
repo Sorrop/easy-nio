345d0b6fcb13a92e738b42ee8f352a5daec0991b
Chris Klaoudianis <chris.klaou@gmail.com>
2026-04-29 21:26:51 +0300

    Make the lib deployment-ready

--- project.clj
+++ project.clj
@@ -1,7 +1,34 @@
-(defproject easy-nio "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "https://example.com/FIXME"
+(defproject net.clojars.sorrop/easy-nio "1.0.0-SNAPSHOT"
+  :description "A collection of clojure wrappers around java.nio facilities"
+  :url "https://github.com/Sorrop/easy-nio"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+
   :dependencies [[org.clojure/clojure "1.12.2"]]
-  :repl-options {:init-ns easy-nio.core})
+
+  :repl-options {:init-ns user}
+
+  :repositories [["releases" {:url   "https://repo.clojars.org"
+                              :creds :gpg}]]
+
+  :plugins [[lein-codox "0.10.8"]
+            [lein-set-version "0.4.1"]]
+
+  :codox {:output-path "docs"
+          :source-uri  "https://github.com/Sorrop/easy-nio/blob/master/{filepath}#L{line}"
+          :namespaces  [easy-nio.buffer
+                        easy-nio.channel
+                        easy-nio.selector
+                        easy-nio.file
+                        easy-nio.protocols]
+          :metadata    {:doc/format :markdown}}
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["codox"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
